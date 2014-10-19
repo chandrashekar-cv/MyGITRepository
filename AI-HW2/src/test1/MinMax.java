e0cf033211e21c4780918293cb823230e718ed75
Chandrashekar Venugopala Chimbili <chandrashekar.cv@outlook.com>
2014-10-19 15:50:22 -0700

    updated minimax - revised from alpha-beta-pruning code. Removed pruning checks to explore all branches.

--- AI-HW2/src/test1/MinMax.java
+++ AI-HW2/src/test1/MinMax.java
@@ -25,93 +25,77 @@ public class MinMax {
 			startMode.currentPositions.add(myAgent.currentPositions.get(i).clone());
 		
 		
-		Node bestMove = MAX(startMode, depth, myAgent, myAgent.myPlayer, myAgent.opponent);
+		Node bestMove = MAX(startMode, depth, myAgent, myAgent.myPlayer, myAgent.opponent, -99999,99999);
 		
 		displayMove(bestMove,log);
 	}
 	
 	
 	
-	Node MAX(Node current, int depth, Agent myAgent,String player, String opponent)
+	Node MAX(Node current, int depth, Agent myAgent,String player, String opponent, int Alpha, int Beta)
 	{
-		
-		
 		PriorityQueue<Node> movesAvail = new PriorityQueue<Node>();
 		
 		List<Node> result = possibleMoves(current,depth+1,myAgent,player,opponent);
 		if(result!=null)
 			movesAvail.addAll(result);
-		
-		Node max = null;
 		Node myMove = null;
-		while(!movesAvail.isEmpty())
+		if(!movesAvail.isEmpty())
 		{
-			AddLog(current, depth);
-			/*
-			this.log = (current.endNode==null)? this.log+"\n"+"root,"+depth+","+
-					((current.score==99999)? "Infinity" :((current.score==-99999)?"-Infinity":current.score)) :
-					this.log+"\n"+((char)(current.endNode[1]+97))+(current.endNode[0]+1)+","+depth+","+
-					((current.score==99999)? "Infinity" : ((current.score==-99999)?"-Infinity":current.score));
-			*/
-			
-					Node next = movesAvail.poll();
-			//Call MIN
-			Node temp = MIN(next, depth+1, myAgent,opponent,player);
-			
-			if(max == null || temp.score > max.score)
+			while(!movesAvail.isEmpty())
 			{
-				max = temp;
-				myMove=next;
-				myMove.Alpha = max.Beta;
-				myMove.score = max.score;
-				current.score = myMove.score;
-			}
-			else if(temp.score == max.score)
-			{
-				if(temp.endNode[0] < max.endNode[0])
-					{
-						max = temp;
-						myMove=next;
-						myMove.Alpha = max.Beta;
-						myMove.score = max.score;
-						current.score = myMove.score;
-						
-					}
-				else if(temp.endNode[0] == max.endNode[0])
+				AddLog(current, depth);
+				Node next = movesAvail.poll();
+		
+					//Call MIN
+				Node temp = MIN(next, depth+1, myAgent,opponent,player,Alpha, Beta);
+				
+				if(current.score < temp.score)
 				{
-					if(temp.endNode[1] < max.endNode[1])
-						{
-							max = temp;
-							myMove=next;
-							myMove.Alpha = max.Beta;
-							myMove.score = max.score;
-							current.score = myMove.score;
-						}
+					current.myMove = next;
+					current.score = temp.score;
 				}
+					
 			}
-		//	Alpha = max.Alpha;
+			
+		}
+		else  //No move found for the current player
+		{
+			if(depth<myAgent.searchCutOffDepth && checkEndGame(current.newStringConfig,player,opponent))
+			{
+				if(current.pass)
+				{
+					return current;
+				}
+				else
+				{
+					AddLog(current, depth);
+					
+					Node temp = new Node();
+					temp .startNode = current.startNode;
+					temp.endNode = current.endNode;
+					temp.gameMode = myAgent.mode;
+					temp.pass = true;
+					temp.score = current.score;
+					temp.copyArray(current,opponent);
+					
+					Node uselessMove = MIN(temp, depth+1, myAgent, opponent, player, Alpha, Beta);
+					
+					if(current.score < uselessMove.score)
+						current.score = uselessMove.score;
+					
+				}
+			
+			}
 			
 		}
 		
 		AddLog(current, depth);
-		/*
-		this.log = (current.endNode==null)? 
-				this.log+"\n"+"root,"+depth+","+((current.score==99999)? "Infinity" : ((current.score==-99999)?"-Infinity":current.score)) :
-				this.log+"\n"+((char)(current.endNode[1]+97))+(current.endNode[0]+1)+","+depth+","+
-				((current.score==99999)? "Infinity" : ((current.score==-99999)?"-Infinity":current.score));
-		*/
+		return current;
 		
-		if(myMove!=null)
-			return myMove;
-		else 
-		{
-			current.Alpha = current.score;
-			current.Beta = current.score;
-			return current;
-		}
 	}
 	
-	Node MIN(Node current, int depth, Agent myAgent, String player, String opponent)
+	Node MIN(Node current, int depth, Agent myAgent, String player, String opponent, int Alpha, int Beta)
 	{
 		PriorityQueue<Node> movesAvail = new PriorityQueue<Node>();
 		
@@ -120,78 +104,79 @@ public class MinMax {
 			movesAvail.addAll(result);
 		
 		
-		Node min=null;
-		Node myMove =null;
-		
-		while(!movesAvail.isEmpty())
+		if(!movesAvail.isEmpty())
 		{
-			AddLog(current, depth);
-			
-			/*this.log = (current.endNode==null)? 
-					this.log+"\n"+"root,"+depth+","+((current.score==99999)? "Infinity" : ((current.score==-99999)?"-Infinity":current.score)) :
-					this.log+"\n"+((char)(current.endNode[1]+97))+(current.endNode[0]+1)+","+depth+","+
-					((current.score==99999)? "Infinity" : ((current.score==-99999)?"-Infinity":current.score));
-			*/
-			
-			Node next = movesAvail.poll();
-			
-			//Call MAX
-			Node temp = MAX(next, depth+1, myAgent, opponent, player);
-			
-			if(min==null || temp.score < min.score)
+			while(!movesAvail.isEmpty())
 			{
-				min = temp;
-				myMove = next;
-				myMove.Beta= min.Alpha;
-				myMove.score = min.score;
-				current.score = myMove.score;
+				AddLog(current, depth);
+				
+				Node next = movesAvail.poll();
+				
+				//Call MAX
+				Node temp = MAX(next, depth+1, myAgent, opponent, player, Alpha, Beta);
+				
+				if(current.score > temp.score)
+					current.score = temp.score;
+				
 			}
-			else if(temp.score == min.score)
+		}
+		else
+		{
+			if(depth<myAgent.searchCutOffDepth && checkEndGame(current.newStringConfig,player,opponent))
 			{
-				if(temp.endNode[0] < min.endNode[0])
+				if(current.pass)
 				{
-					min = temp;
-					myMove=next;
-					myMove.Beta= min.Alpha;
-					myMove.score = min.score;
-					current.score = myMove.score;
+					return current;
 				}
-				else if(temp.endNode[0] == min.endNode[0])
+				else
 				{
-					if(temp.endNode[1] < min.endNode[1])
-						{
-							min = temp;
-							myMove=next;
-							myMove.Beta = min.Alpha;
-							myMove.score = min.score;
-							current.score = myMove.score;
-						}
+
+					AddLog(current, depth);
+					
+					Node temp = new Node();
+					temp .startNode = current.startNode;
+					temp.endNode = current.endNode;
+					temp.gameMode = myAgent.mode;
+					temp.pass = true;
+					temp.score = current.score;
+					temp.copyArray(current,opponent);
+					
+					Node uselessMove  = MAX(temp, depth+1, myAgent, opponent, player, Alpha, Beta);
+					
+					if(current.score > uselessMove.score)
+						current.score = uselessMove.score;
+					
 				}
 			}
 			
-		//	Beta = min.Beta;
 		}
 		
 		AddLog(current, depth);
-		
-		/*this.log = (current.endNode==null)? 
-				this.log+"\n"+"root,"+depth+","+((current.score==99999)? "Infinity" : ((current.score==-99999)?"-Infinity":current.score)) :
-				this.log+"\n"+((char)(current.endNode[1]+97))+(current.endNode[0]+1)+","+depth+","+
-				((current.score==99999)? "Infinity" : ((current.score==-99999)?"-Infinity":current.score));
-		*/
-		
-		if(myMove!=null)
-			return myMove;
-		else 
-		{
-			current.Alpha = current.score;
-			current.Beta = current.score;
-			return current;
-		}
+		return current;
+	
 	}
 	
 	
-	
+	 static boolean checkEndGame(String[][] state,String player, String opponent)
+	{
+		boolean pl=false;
+		boolean op = false;
+		boolean fullBoard = true;
+		
+		for(int i=0;i<state.length;i++)
+			for(int j=0;j<state.length;j++)
+			{
+				if(!pl)
+					{pl = (!pl && state[i][j].equalsIgnoreCase(player))? true: false;}
+				if(!op)
+					{op = (!op && state[i][j].equalsIgnoreCase(opponent))? true: false;}
+				
+				if(state[i][j].equals("*"))
+					fullBoard = false;
+			}
+				
+		return (!fullBoard && op && pl);
+	}
 	
 	/*
 	 * Identify the possible moves in the current board configuration.
@@ -216,50 +201,50 @@ public class MinMax {
 					switch(k)
 					{
 						
-						case 0: if(i>1 && j>1 && myAgent.dataSet[i-1][j-1]>=0)
+						case 0: if(i>1 && j>1 && node.newConfig[i-1][j-1]>=0)
 								{
 									newPosition = searchPosition(node, myAgent,i-1,j-1,k,player);
 								}
 								break;
 						
-						case 1: if(i>1 && myAgent.dataSet[i-1][j]>=0)
+						case 1: if(i>1 && node.newConfig[i-1][j]>=0)
 								{
 									newPosition = searchPosition(node, myAgent,i-1,j,k,player);
 								}
 								break;
 						
-						case 2:	if(i>1 && j<6 && myAgent.dataSet[i-1][j+1]>=0)
+						case 2:	if(i>1 && j<6 && node.newConfig[i-1][j+1]>=0)
 								{
 									newPosition = searchPosition(node, myAgent,i-1,j+1,k,player);
 								}
 								break;
 						
-						case 3:	if(j>1 && myAgent.dataSet[i][j-1]>=0)
+						case 3:	if(j>1 && node.newConfig[i][j-1]>=0)
 								{
 									newPosition = searchPosition(node, myAgent,i,j-1,k,player);
 								}
 								break;
 				
-						case 4:	if(j <6 && myAgent.dataSet[i][j+1]>=0)
+						case 4:	if(j <6 && node.newConfig[i][j+1]>=0)
 								{
 									newPosition = searchPosition(node, myAgent,i,j+1,k,player);
 									
 								}
 								break;
 								
-						case 5: if(i<6 && j>1 && myAgent.dataSet[i+1][j-1]>=0)
+						case 5: if(i<6 && j>1 && node.newConfig[i+1][j-1]>=0)
 								{
 									newPosition = searchPosition(node, myAgent,i+1,j-1,k,player);
 								}
 								break;
 				
-						case 6: if(i<6 && myAgent.dataSet[i+1][j]>=0)
+						case 6: if(i<6 && node.newConfig[i+1][j]>=0)
 								{
 									newPosition = searchPosition(node, myAgent,i+1,j,k,player);
 								}
 								break;
 					
-						case 7: if(i<6 && j<6 && myAgent.dataSet[i+1][j+1]>=0)
+						case 7: if(i<6 && j<6 && node.newConfig[i+1][j+1]>=0)
 								{
 									newPosition = searchPosition(node, myAgent,i+1,j+1,k,player);
 								}
@@ -273,6 +258,8 @@ public class MinMax {
 						temp.endNode = newPosition.clone();
 						temp.mode = k;
 						temp.gameMode = myAgent.mode;
+						temp.Alpha = node.Alpha;
+						temp.Beta = node.Beta;
 						temp.calculateScore(node, myAgent,player,opponent,depth);
 						
 						positions.add(temp);
@@ -305,13 +292,13 @@ public class MinMax {
 		{
 			if(node.newConfig[m][n]>=0)
 			{
-				if((player.equalsIgnoreCase("x") && node.newConfig[m][n]==0)  ||
-						(player.equalsIgnoreCase("o") && node.newConfig[m][n]==1))
+				if((player.equalsIgnoreCase("X") && node.newConfig[m][n]==0)  ||
+						(player.equalsIgnoreCase("O") && node.newConfig[m][n]==1))
 				{
 					oppCoins=true;
 				}
-				if(oppCoins && (player.equalsIgnoreCase("o") && node.newConfig[m][n]==0)  ||
-						(player.equalsIgnoreCase("1") && node.newConfig[m][n]==1))
+				else if((player.equalsIgnoreCase("O") && node.newConfig[m][n]==0)  ||
+						(player.equalsIgnoreCase("X") && node.newConfig[m][n]==1))
 				{
 					//No move as player coin appears after opponent coins before an empty location
 					break;
@@ -361,14 +348,22 @@ public class MinMax {
 
 	void displayMove(Node node, String log)
 	{
+		String [][] state = null;
+		
+		if(node.myMove==null)
+			state = node.newStringConfig;
+		else
+			state = node.myMove.newStringConfig;
+		
 		for(int i=0;i<8;i++)
 		{
 			String temp ="";
 			for(int j=0;j<8;j++)
-				temp+=node.newStringConfig[i][j]+"	";
+				temp+=state[i][j]+"	";
 			
 			System.out.println(temp);
 		}
+		
 		System.out.println(log);
 	}
 	
